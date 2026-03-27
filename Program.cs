2e98c2f4cb0fa9326578da3edc9839ff29e11c9e
Thanh Binh <thanhbinh@MacBook-Pro-cua-Thanh.local>
2026-03-27 23:36:59 +0700

    okoko213

--- Program.cs
+++ Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddScoped<FileStorageService>();
 
 builder.Services.AddSingleton<TestIdOption>();
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+
+    Console.WriteLine("DB Path: " + db.Database.GetDbConnection().DataSource);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
